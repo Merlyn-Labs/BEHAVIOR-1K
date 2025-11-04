02d39ba89f690e62ba4379e69ce228609b956ad6
Salman Shahid <salmanshahidzia@gmail.com>
2025-11-04 00:47:42 +0000

    finally fix DS replay

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -17,10 +17,9 @@ XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.p
     ${POLICY_ARGS} \
     task.name=picking_up_trash \
     eval_on_train_instances=true \
-    eval_instance_ids=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] \
-    max_steps=1000 \
-    extra_notes="The goal here is to see if the policy can use its arms when given a very low-level prompt." \
-    log_path="${LOG_DIR}/${EXP_NAME}_tor_test" \
+    eval_instance_ids=[0,1,2] \
+    extra_notes="Just reproducing some pick up trash results" \
+    log_path="${LOG_DIR}/${EXP_NAME}_put_train" \
     # prompt="\"Spin around and around in circles.\""
 
 # XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
