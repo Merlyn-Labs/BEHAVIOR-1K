469a6af7631223d8b984ce0122d4b77066ec1cfe
Salman Shahid <salmanshahidzia@gmail.com>
2025-11-09 20:05:13 +0000

    committing latest experiment

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -10,41 +10,24 @@ conda activate behavior
 # POLICY_ARGS="policy=websocket"
 
 # –––––– Model 1 ––––––
-EXP_NAME="good_rl_40_step"
+aws s3 sync \
+    s3://behavior-challenge/outputs/checkpoints/pi0_b1k_2nd/openpi_0_20251109_050108/33000/ \
+    /workspace/openpi/outputs/checkpoints/pi0_b1k_2nd/openpi_0_20251109_050108/33000/
+
+EXP_NAME="pi0_on_3_tasks"
 LOG_DIR="video_outputs/${EXP_NAME}"
 
 mkdir -p "${LOG_DIR}"
 
-POLICY_ARGS="policy=local policy_config=pi05_b1k policy_dir=/workspace/openpi/logs/20251108_022813/test_openpi_pi05_behavior/checkpoints/global_step_40/actor/"
-
-XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
-    ${POLICY_ARGS} \
-    task.name=picking_up_trash \
-    eval_on_train_instances=false \
-    eval_instance_ids=[0,1,2] \
-    log_path="${LOG_DIR}/${EXP_NAME}_put_test"
+POLICY_ARGS="policy=local policy_config=pi0_b1k_2nd policy_dir=/workspace/openpi/outputs/checkpoints/pi0_b1k_2nd/openpi_0_20251109_050108/33000/"
 
 XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
     ${POLICY_ARGS} \
     task.name=turning_on_radio \
     eval_on_train_instances=false \
-    eval_instance_ids=[0,1,2,3,4] \
+    eval_instance_ids=[0,1,2,3] \
+    use_heavy_robot=true \
     log_path="${LOG_DIR}/${EXP_NAME}_tor_test"
-
-# XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
-#     ${POLICY_ARGS} \
-#     task.name=freeze_pies \
-#     eval_on_train_instances=false \
-#     eval_instance_ids=[0,1,2,3] \
-#     log_path="${LOG_DIR}/${EXP_NAME}_fp_test"
-
-# –––––– Model 2 ––––––
-EXP_NAME="simple_run_arms_ds_filtering_no_weighting"
-LOG_DIR="video_outputs/${EXP_NAME}"
-
-mkdir -p "${LOG_DIR}"
-
-POLICY_ARGS="policy=local policy_config=pi05_b1k policy_dir=/workspace/openpi/outputs/checkpoints/pi05_b1k/openpi_05_20251108_052323/16000/"
 
 XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
     ${POLICY_ARGS} \
@@ -54,18 +37,58 @@ XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.p
     log_path="${LOG_DIR}/${EXP_NAME}_put_test" \
     use_heavy_robot=true
 
+# –––––– Model 2 ––––––
+aws s3 sync \
+    s3://behavior-challenge/outputs/checkpoints/pi05_b1k_2nd/openpi_05_20251109_031930/36000/ \
+    /workspace/openpi/outputs/checkpoints/pi05_b1k_2nd/openpi_05_20251109_031930/36000/
+
+EXP_NAME="pi05_from_scratch_on_3_tasks"
+LOG_DIR="video_outputs/${EXP_NAME}"
+
+mkdir -p "${LOG_DIR}"
+
+POLICY_ARGS="policy=local policy_config=pi05_b1k_2nd policy_dir=/workspace/openpi/outputs/checkpoints/pi05_b1k_2nd/openpi_05_20251109_031930/36000/"
+
 XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
     ${POLICY_ARGS} \
     task.name=turning_on_radio \
     eval_on_train_instances=false \
-    eval_instance_ids=[0,1,2,3,4] \
+    eval_instance_ids=[0,1,2,3] \
     use_heavy_robot=true \
     log_path="${LOG_DIR}/${EXP_NAME}_tor_test"
 
 XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
     ${POLICY_ARGS} \
-    task.name=freeze_pies \
+    task.name=picking_up_trash \
+    eval_on_train_instances=true \
+    eval_instance_ids=[0,1,2] \
+    log_path="${LOG_DIR}/${EXP_NAME}_put_test" \
+    use_heavy_robot=true
+
+# –––––– Model 3 ––––––
+aws s3 sync \
+    s3://behavior-challenge/outputs/checkpoints/pi05_b1k/openpi_05_20251109_025919/33000/ \
+    /workspace/openpi/outputs/checkpoints/pi05_b1k/openpi_05_20251109_025919/33000/
+
+EXP_NAME="pi05_continued_on_3_tasks"
+LOG_DIR="video_outputs/${EXP_NAME}"
+
+mkdir -p "${LOG_DIR}"
+
+POLICY_ARGS="policy=local policy_config=pi05_b1k policy_dir=/workspace/openpi/outputs/checkpoints/pi05_b1k/openpi_05_20251109_025919/33000/"
+
+XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
+    ${POLICY_ARGS} \
+    task.name=turning_on_radio \
     eval_on_train_instances=false \
     eval_instance_ids=[0,1,2,3] \
     use_heavy_robot=true \
-    log_path="${LOG_DIR}/${EXP_NAME}_fp_test"
+    log_path="${LOG_DIR}/${EXP_NAME}_tor_test"
+
+XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
+    ${POLICY_ARGS} \
+    task.name=picking_up_trash \
+    eval_on_train_instances=true \
+    eval_instance_ids=[0,1,2] \
+    log_path="${LOG_DIR}/${EXP_NAME}_put_test" \
+    use_heavy_robot=true
